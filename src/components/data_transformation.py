a3682aa770febe526b29f62f679655b7a01cf5fb
Abdelrahman Farouk <abdelrahman.farouk56@gmail.com>
2025-11-14 18:41:21 +0200

    prdection pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,7 +16,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts', 'proprocessor.pkl')
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
@@ -38,7 +38,7 @@ class DataTransformation:
 
                 cat_pipeline = Pipeline( steps=[
                     ('imputer', SimpleImputer(strategy='most_frequent')),
-                    ('one_hot_encoder', OneHotEncoder()),
+                    ('one_hot_encoder', OneHotEncoder(handle_unknown='ignore')),
                     ('scaler', StandardScaler(with_mean=False))]
 
                 )
